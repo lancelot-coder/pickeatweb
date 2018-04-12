4dd43584c87be83b07efd15f3598fbc404b4d71e
lancelot-coder <johnharveypanganibanofficial@gmail.com>
2018-04-13 05:33:43 +0800

    added verified eatery logo in show page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     published_restaurants = Restaurant.published
     @restaurants = published_restaurants.last(5)
+    @numberofrestaurants = published_restaurants.last(8)
     @random_eateries = published_restaurants.order("RANDOM()").last(8)
   end
 end
