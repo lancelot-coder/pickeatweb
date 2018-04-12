ce483ed1e2e9d554154f645571fa8dbb297c33f3
Ayra Obazee <ayra.panganiban@gmail.com>
2018-04-13 04:08:00 +0800

    [ENH] randomize resto

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @restaurants = Restaurant.published.last(5)
+    published_restaurants = Restaurant.published
+    @restaurants = published_restaurants.last(5)
+    @random_eateries = published_restaurants.order("RANDOM()").last(8)
   end
-
 end
