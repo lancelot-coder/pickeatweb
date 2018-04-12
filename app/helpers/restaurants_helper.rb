9ea20975c0648c3fe38b1a5ae5827f98bb5dc0f5
Ayra Obazee <ayra.panganiban@gmail.com>
2018-04-12 12:49:37 +0800

    [ENH] ratings

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -11,4 +11,9 @@ module RestaurantsHelper
     query_string = params.map{|k,v| "#{k}=#{v}"}.join("&")
     return "https://www.google.com/maps/embed/v1/place?#{query_string}"
   end
+
+  def user_rated?(user, restaurant)
+    rate = Rate.find_by(rater: user, rateable: restaurant)
+    true if rate.present?
+  end
 end
