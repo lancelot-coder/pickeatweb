81c840c0230a98db58b5a87b7161d561c13a9fca
lancelot-coder <johnharveypanganibanofficial@gmail.com>
2018-04-13 10:46:51 +0800

    add about us in contacts

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,15 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'about' => 'pages#about', as: 'about'
-  get 'properties-grid' => 'pages#properties-grid', as: 'properties-grid'
-  get 'properties-single' => 'pages#properties-single', as: 'properties-single'
-  get 'team' => 'pages#team', as: 'team'
   get 'team-single' => 'contacts#team-single', as: 'team-single'
   root to: "home#index"
 
   devise_for :users
 
-  resources :restaurants do 
+  resources :restaurants do
     resources :restaurant_photos
   end
   resources :contacts
   resources :reviews
-  
+
 end
