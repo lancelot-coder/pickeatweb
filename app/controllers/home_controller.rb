b694ff8860720d6d3012a34ba3fd1d7210e1d980
lancelot-coder <johnharveypanganibanofficial@gmail.com>
2018-04-19 11:46:46 +0800

    fixed names

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     published_restaurants = Restaurant.published
-    @restaurants = published_restaurants.last(2)
-    @numberofrestaurants = published_restaurants.last(8)
+    @restaurants = published_restaurants.last(8)
+    @numberofrestaurants = published_restaurants.last(2)
     @random_eateries = published_restaurants.order("RANDOM()").last(8)
   end
 end
