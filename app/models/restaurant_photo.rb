00dc65285257b06211db691f10c4f79afec92a17
Ayra Obazee <ayra.panganiban@gmail.com>
2018-04-19 10:15:39 +0800

    [ENH] config s3

--- app/models/restaurant_photo.rb
+++ app/models/restaurant_photo.rb
@@ -1,7 +1,7 @@
 class RestaurantPhoto < ApplicationRecord
   belongs_to :restaurant
 
-  has_attached_file :photo, styles: { thumb: "100x100>", big: "500x500>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo, styles: { thumb: "100x100>", big: "500x500>" }, default_url: "//s3.amazonaws.com/pickeat/eateries/default_photos/default-thumbnail.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
   scope :not_primary, -> { where(primary: false) }
