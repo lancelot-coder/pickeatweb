4370d114d85fd83979ad8e858b6f93de18dc4773
lancelot-coder <johnharveypanganibanofficial@gmail.com>
2018-04-19 11:50:06 +0800

    verified resto in home page lie

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   def index
     published_restaurants = Restaurant.published
     @restaurants = published_restaurants.last(8)
-    @numberofrestaurants = published_restaurants.last(2)
     @random_eateries = published_restaurants.order("RANDOM()").last(8)
   end
 end
